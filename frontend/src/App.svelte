c8b0f25c53807c0ef371d0dac6192cf06203f7dd
twsnmp <ymi@twise.co.jp>
2022-01-08 09:32:22 +0900

    wails initで生成したソースコードを組み込む

---
+++ frontend/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  let name = "";
+  let greeting = "";
+
+  function greet() {
+    window.go.main.App.Greet(name).then((result) => {
+      greeting = result;
+    });
+  }
+</script>
+
+<main>
+  <div id="input" data-wails-no-drag>
+    <input id="name" type="text" bind:value={name} />
+    <button class="button" on:click={greet}>Greet</button>
+  </div>
+  {#if greeting}
+    <div id="result">{greeting}</div>
+  {/if}
+</main>
+
+<style>
+  main {
+    height: 100%;
+    width: 100%;
+    margin:40px auto;
+  }
+
+  #result {
+    margin-top: 1rem;
+    font-size: 1.5rem;
+    color: azure;
+  }
+
+  button {
+    -webkit-appearance: default-button;
+    padding: 6px;
+  }
+
+  #name {
+    border-radius: 3px;
+    outline: none;
+    -webkit-font-smoothing: antialiased;
+  }
+</style>
