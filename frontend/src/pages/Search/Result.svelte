ebda8ca0e0aa8e6f1b8e347e8e557e3f917644fe
twsnmp <ymi@twise.co.jp>
2022-02-25 05:56:07 +0900

    読み込み結果画面の改善

--- frontend/src/pages/Search/Result.svelte
+++ frontend/src/pages/Search/Result.svelte
@@ -2,6 +2,12 @@
   import { X16 } from "svelte-octicons";
   import { createEventDispatcher } from "svelte";
   import { onMount } from 'svelte';
+  import {
+    getFieldName,
+    getFieldType,
+    getFieldUnit,
+    isFieldValid,
+  } from "../../js/define";
 
   export let indexInfo;
   const dispatch = createEventDispatcher();
@@ -38,60 +44,72 @@
         {errorMsg}
       </div>
     {/if}
-    <div class="Box-body">
-      <div class="markdown-body">
-        <table>
-          <tbody>
-            <tr>
-              <th>総数</th>
-              <td>{indexInfo.Total}</td>
-            </tr>
-            <tr>
-              <th>処理時間</th>
-              <td>{indexInfo.Duration}</td>
-            </tr>
-            <tr>
-              <th>抽出項目</th>
-              <td>
-                {#each indexInfo.Fields as f,i}
-                  {f}
-                  {#if (i+1) % 10 == 0}
-                    <br>
-                  {:else}
-                    ,
-                  {/if}
-                {/each}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="mt-2 markdown-body">
-        <table>
+    <div class="Box-row markdown-body log">
+      <h5>概要</h5>
+      <table>
+        <tbody>
+          <tr>
+            <th>総数</th>
+            <td>{indexInfo.Total}</td>
+          </tr>
+          <tr>
+            <th>処理時間</th>
+            <td>{indexInfo.Duration}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    {#if indexInfo.Fields.length > 0}
+      <div class="Box-row markdown-body log">
+        <h5>抽出した項目</h5>
+        <table class="fields">
           <thead>
             <tr>
-              <th>有効率</th>
-              <th>完了</th>
-              <th>対象</th>
-              <th>処理時間</th>
-              <th>サイズ</th>
-              <th>パス</th>
+              <th>変数名</th>
+              <th>名前</th>
+              <th>種別</th>
+              <th>単位</th>
             </tr>
           </thead>
           <tbody>
-          {#each logFiles as f }
-            <tr>
-              <td class:color-fg-danger={(f.Read ? (100.0 * f.Send/f.Read) : 100) < 50.0}>{f.Read ? (100.0 * f.Send/f.Read).toFixed(2) : 0}%</td>
-              <td>{f.Read}</td>
-              <td>{f.Send}</td>
-              <td>{f.Duration}</td>
-              <td>{f.Size}</td>
-              <td>{f.Path}</td>
-            </tr>
-          {/each}
+            {#each indexInfo.Fields as f}
+              <tr>
+                <td>{f}</td>
+                <td class:color-fg-danger={!isFieldValid(f)}>{getFieldName(f)}</td>
+                <td>{getFieldType(f)}</td>
+                <td>{getFieldUnit(f)}</td>
+              </tr>
+            {/each}
           </tbody>
         </table>
       </div>
+    {/if}
+    <div class="Box-row markdown-body log">
+      <h5>読み込んだファイル</h5>
+      <table>
+        <thead>
+          <tr>
+            <th>有効率</th>
+            <th>完了</th>
+            <th>対象</th>
+            <th>処理時間</th>
+            <th>サイズ</th>
+            <th>パス</th>
+          </tr>
+        </thead>
+        <tbody>
+        {#each logFiles as f }
+          <tr>
+            <td class:color-fg-danger={(f.Read ? (100.0 * f.Send/f.Read) : 100) < 50.0}>{f.Read ? (100.0 * f.Send/f.Read).toFixed(2) : 0}%</td>
+            <td>{f.Read}</td>
+            <td>{f.Send}</td>
+            <td>{f.Duration}</td>
+            <td>{f.Size}</td>
+            <td>{f.Path}</td>
+          </tr>
+        {/each}
+        </tbody>
+      </table>
     </div>
     <div class="Box-footer text-right">
       <button class="btn" type="button" on:click={back}>
