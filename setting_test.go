c454605e62dff2d48bc03bf65b041e048f283277
twsnmp <ymi@twise.co.jp>
2023-05-13 06:04:16 +0900

    findGrok関数のFuzzingテストを追加

--- setting_test.go
+++ setting_test.go
@@ -21,3 +21,23 @@ func FuzzFindSplunkPat(f *testing.F) {
 		}
 	})
 }
+
+func FuzzFindGrok(f *testing.F) {
+	f.Add(" 192.168.1.1 ")
+	f.Add(" http://www.twise.co.jp ")
+	f.Add(" 01:02:03:04:05:06 ")
+	f.Add(" twsmmp@gmail.com ")
+	f.Fuzz(func(t *testing.T, td string) {
+		rmap := make(map[string]string)
+		for f, ps := range grokTestMap {
+			findGrok(f, td, ps, rmap)
+		}
+		if len(rmap) > 0 {
+			for k := range rmap {
+				if !strings.Contains(td, k) {
+					t.Fatalf("failed td='%s' k='%s' rmap=%+v", td, k, rmap)
+				}
+			}
+		}
+	})
+}
